0af325964cc665e0dfa60cbb13309c007c5816a9
Julian Trajanson <julian.trajanson@gmail.com>
2017-12-20 23:11:24 -0500

    extract server

--- src/chatbot/index.ts
+++ src/chatbot/index.ts
@@ -3,6 +3,6 @@ import { ChatConnector } from "botbuilder";
 
 export const injectBot = (connector: ChatConnector) => {
   const bot = new builder.UniversalBot(connector, (session) => {
-    session.send("Hi from new order");
+    session.send("Hi from new order with extracted server");
   });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,12 @@
 import * as builder from "botbuilder";
 
-import * as express from "express";
-
-import * as errorHandler from "errorhandler";
-
 import { injectBot } from "./chatbot";
-
-// const app = require("./app");
-
-// Create Express server
-const app = express();
-
-/**
- * Error Handler. Provides full stack - remove for production
- */
-// app.use(errorHandler());
-
-/**
- * Start Express server.
- */
-const server = app.listen(process.env.PORT, () => {
-  console.log(("  App is running at http://localhost:%d in %s mode"), process.env.PORT, process.env.ENV);
-  console.log("  Press CTRL-C to stop\n");
-});
-
-export = server;
+import { app } from "./appServer";
 
 const connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD,
   openIdMetadata: process.env.BotOpenIdMetadata,
-});
-
-app.get("/", (req, res) => {
-  res.write("(200)\nSite is operational.");
-  res.end();
 });
 
 app.post("/api/messages", connector.listen());
