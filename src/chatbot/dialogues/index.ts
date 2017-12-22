c2cd4a0bad3abd804bec7faf30fcc3303bfe371c
Julian Trajanson <julian.trajanson@gmail.com>
2017-12-21 23:25:45 -0500

    extract main dialogue

--- src/chatbot/dialogues/index.ts
+++ src/chatbot/dialogues/index.ts
@@ -2,6 +2,21 @@ import * as builder from "botbuilder";
 import { UniversalBot } from "botbuilder";
 
 export const injectDialogues = (bot: UniversalBot) => {
+    bot.dialog("/", [(session, args, next) => {
+        const userAddress = session.message.address;
+
+        session.send("The bot is working");
+        session.send(`Your address is ${JSON.stringify(userAddress)}`);
+
+        const isKnownUser = false;
+        if (isKnownUser) {
+          session.beginDialog("knownUser");
+        } else {
+          session.beginDialog("unknownUser");
+        }
+        session.endConversation(`That's all for now! Let's talk soon!`);
+    }]);
+
     bot.dialog("knownUser", [(session, args, next) => {
         session.send("Hey Theoderik! Have we met? I’m Taylor!");
         // session.endDialogWithResult({ response: name.trim() });
--- src/chatbot/index.ts
+++ src/chatbot/index.ts
@@ -7,33 +7,8 @@ import { injectDialogues } from "./dialogues";
 export const injectBot = (route: string, app: Express, connector: ChatConnector) => {
   app.post(route, connector.listen());
 
-  const bot = new builder.UniversalBot(connector, [(session, args, next) => {
-    const userAddress = session.message.address;
-
-    session.send("The bot is working");
-    session.send(`Your address is ${JSON.stringify(userAddress)}`);
-
-    const isKnownUser = false;
-    if (isKnownUser) {
-      session.beginDialog("knownUser");
-    } else {
-      session.beginDialog("unknownUser");
-    }
-    session.endConversation(`That's all for now! Let's talk soon!`);
-  }]);
-
-  bot.dialog("knownUser", [(session, args, next) => {
-    session.send("Hey Theoderik! Have we met? I’m Taylor!");
-    // session.endDialogWithResult({ response: name.trim() });
-  session.endDialogWithResult({});
-  }]);
-
-  bot.dialog("unknownUser", [(session, args, next) => {
-    session.send(`Hey Theoderik! Have we met? I’m Taylor!`);
-    session.send(`I help Julian schedule time to assemble with superlative people like yourself. In fact, that's what I'm doing right now! Are you free Tuesday or Wednesday at 7pm EST?`);
-    session.endDialogWithResult({});
-  }]);
-
+  const bot = new builder.UniversalBot(connector);
+  injectDialogues(bot);
 
   // send simple notification
   const sendProactiveMessage = (address: IAddress) => {
